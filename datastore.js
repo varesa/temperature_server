a67daea7e597b20771039c12b48a73512bdef365
Esa Varemo <esa@kuivanto.fi>
2015-10-24 23:54:08 +0300

    Remove unused datastore.js methods. Will be reimplemented if necessary

--- datastore.js
+++ datastore.js
@@ -10,51 +10,6 @@ Datastore.prototype.push = function (id, value) {
     });
 };
 
-/*// Return the last added record for a sensor
-Datastore.prototype.getLatestBySensor = function (id, cb) {
-    models.Record.findOne({
-        where: {
-            deviceId: id
-        },
-        order: 'date DESC'
-    }).then(cb).catch(function (err) {
-        console.log(err);
-    });
-};*/
-
-/*// Record x * latest records for a sensor
-Datastore.prototype.getLimitBySensor = function (id, limit, cb) {
-    models.Record.findAll({
-        where: {
-            deviceId: id
-        },
-        order: 'date DESC',
-        limit: limit
-    }).then(cb).catch(function (err) {
-        console.log(err);
-    });
-};*/
-
-/*// Return x * latest records (not per-sensor)
-Datastore.prototype.getLimitByTotal = function (limit, cb) {
-    models.Record.findAll({
-        order: 'date DESC',
-        limit: limit
-    }).then(cb).catch(function (err) {
-        console.log(err);
-    });
-};*/
-
-/*Datastore.prototype.getAllBySensor = function (id, cb) {
-    models.Record.findAll({
-        where: {
-            deviceId: id
-        }
-    }).then(cb).catch(function (err) {
-        console.log(err);
-    });
-};*/
-
 Datastore.prototype.getAll = function(cb) {
     models.Record.findAll({
         order: "date ASC"
